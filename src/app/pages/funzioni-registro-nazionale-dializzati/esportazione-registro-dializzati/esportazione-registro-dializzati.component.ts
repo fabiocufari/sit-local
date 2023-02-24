9dd75665eb22a788f4ffbce4da5d228b7e70c1d1
MAINSOFTLAB\antonio.bertucci <antonio.bertucci@soft.it>
2023-02-24 11:05:28 +0100

    Fatte tutte le pagine registro dializzati vari fix

---
+++ src/app/pages/funzioni-registro-nazionale-dializzati/esportazione-registro-dializzati/esportazione-registro-dializzati.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-esportazione-registro-dializzati',
+  templateUrl: './esportazione-registro-dializzati.component.html',
+  styleUrls: ['./esportazione-registro-dializzati.component.scss']
+})
+export class EsportazioneRegistroDializzatiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/funzioni-registro-nazionale-dializzati/funzioni-registro-nazionale-dializzati-routing.module.ts
+++ src/app/pages/funzioni-registro-nazionale-dializzati/funzioni-registro-nazionale-dializzati-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AcquisizioneDatiRegistroRegionaleComponent } from './acquisizione-dati-registro-regionale/acquisizione-dati-registro-regionale.component';
 import { ConsultazioneEsitoAcquisizioneComponent } from './consultazione-esito-acquisizione/consultazione-esito-acquisizione.component';
+import { EsportazioneRegistroDializzatiComponent } from './esportazione-registro-dializzati/esportazione-registro-dializzati.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
   path: 'consultazione-esito-acquisizione',
   component: ConsultazioneEsitoAcquisizioneComponent
+  },
+  {
+  path: 'esportazione-registro-dializzati',
+  component: EsportazioneRegistroDializzatiComponent
   },
   {
   path: '',
