dc9adddfadb36093877dfee35150e2008aabb6c3
fabiocufari <fabio.cufari@gmail.com>
2023-03-07 18:40:22 +0100

    fix tooltip in table

--- src/app/shared/components/custom-table/custom-table.component.ts
+++ src/app/shared/components/custom-table/custom-table.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
 import { colType } from './colType.enum';
+declare var bootstrap: any;
 @Component({
   selector: 'app-custom-table',
   templateUrl: './custom-table.component.html',
   styleUrls: ['./custom-table.component.scss']
 })
-export class CustomTableComponent {
+export class CustomTableComponent implements AfterViewInit{
   @Input() headers: any[] = [];
   @Input() data: any[] = [];
 
@@ -13,10 +14,17 @@ export class CustomTableComponent {
 
   colType=colType
   
-
-
+  ngAfterViewInit() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+    })     
+  }
+  ngOnInit(){
+  
+  }
   buttonClick(row: any,action :string){
     this.clickEvent.emit({id : row,action :action})
   }
-
+  
 }
